002196ce4a4033bce487137a50cfa491e8505432
Daniel Gebhard <daniel.gebhard00@gmx.de>
2023-03-12 18:08:25 -0700

    implemented resume training functionality

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 """
 This file implements the training functionality. It gets passed the name of a configuration as an argument.
+Usage:
+1) train new agent based on config: python main.py --config default
+2) load most recent version of an agent: python main.py --load PPO-MlpPolicy
+3) load specific version of an agent: python main.py --load PPO-MlpPolicy-0 (this overrides previous training and
+might lead to intended behavior if less than already available epochs are retrained)
 """
-import sys
+import os
 import json
 from model_factory import get_model
 from environment_factory import get_environment
+import argparse
+import shutil
 
 
 if __name__ == "__main__":
-    config_name = 'default'
+    parser = argparse.ArgumentParser(description='CSE151B Project')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--config', type=str, help='configuration to train a new agent on')
+    group.add_argument('--load', type=str, help='name of the model to load')
+    args = parser.parse_args()
 
-    if len(sys.argv) > 1:
-        config_name = sys.argv[1]
+    if args.config is not None:
+        config_name = args.config
+        agent_name = None
+    elif args.load is not None:
+        agent_name = args.load
+        if agent_name[agent_name.rfind('-') + 1:].isdigit():
+            config_name = os.path.join("agents", agent_name[:agent_name.rfind('-')], "config")
+        else:
+            config_name = os.path.join("agents", agent_name, "config")
+    else:
+        raise ValueError("Either --config or --load argument has to be passed")
 
     with open(config_name + ".json") as json_file:
         config = json.load(json_file)
+    if args.config is not None:
+        os.mkdir(os.path.join("agents", config['model']['name']))
+        shutil.copy2(config_name + ".json", os.path.join("agents", config['model']['name'], "config.json"))
 
     env = get_environment(config)
-    model = get_model(env, config)
+    model, number_of_previous_epochs = get_model(env, config, agent_name)
     for epoch in range(config['training']['epochs']):
         model.learn(config['training']['timesteps_per_epoch'])
-        model.save("agents/" + config['model']['name'] + "-{}".format(epoch))
+        model.save(os.path.join("agents",config['model']['name'], config['model']['name'] + "-{}".format(
+            epoch + number_of_previous_epochs)))
--- model_factory.py
+++ model_factory.py
@@ -2,16 +2,63 @@
 This file implements the get_model function used in main.py
 """
 from stable_baselines3 import PPO
+import os
 
 
-def get_model(env, config):
+def get_number_of_trained_epochs(agent_name):
+    """
+    :param agent_name: string containing the name of the agent
+    :return number_of_previous_epochs: the number of epochs the model was trained for before
+    """
+    number_of_previous_epochs = 0
+    for file in os.listdir(os.path.join("agents", agent_name)):
+        if agent_name in file:
+            epoch_number = int(file[file.rfind('-') + 1:-4])
+            if epoch_number > number_of_previous_epochs:
+                number_of_previous_epochs = epoch_number
+    return number_of_previous_epochs
+
+def get_model(env, config, agent_name):
     """
     :param env: a gym environment
     :param config: a dictionary holding the configuration specified in the json-file
+    :param agent_name: the name of an agent to be loaded
     :return model: a gym model for the given environment, following the specifications given in config
+    :return number_of_previous_epochs: the number of previously trained epochs for loaded model; 0 for new model
     """
     type = config['model']['type']
-    if type == "PPO":
-        policy = config['model']['policy']
-        model = PPO(policy, env)
-    return model
+    if agent_name is not None:
+        if agent_name[agent_name.rfind('-') + 1:].isdigit():
+            number_of_previous_epochs = int(agent_name[agent_name.rfind('-') + 1:])
+            if get_number_of_trained_epochs(agent_name[:agent_name.rfind('-')]) \
+                    > number_of_previous_epochs + config['training']['epochs']:
+                choice = input(
+                    "You are about to retrain an agent from a given number of epochs which is lower than the\n"
+                    "one the model was trained on previously. This will overwrite states of the agents.\n"
+                    "Additionally, the total number of epochs after training will be lower than the number\n"
+                    "of total epochs the model was trained on before. This might lead to unintended behavior\n"
+                    "because there will be a cutoff point in the saved agents. Please consider if you really\n"
+                    "want to do this. Confirm with [y] or cancel with any other input.\n")
+                if choice != 'y':
+                    exit()
+            elif get_number_of_trained_epochs(agent_name[:agent_name.rfind('-')]) > number_of_previous_epochs:
+                choice = input(
+                    "You are about to retrain an agent from a given number of epochs which is lower than the\n"
+                    "one the model was trained on previously. This will overwrite states of the agents.\n"
+                    "Please consider if you really want to do this. Confirm with [y] or cancel with any other input.\n")
+                if choice != 'y':
+                    exit()
+            agent_name = os.path.join(agent_name[:agent_name.rfind('-')], agent_name)
+        else:
+            number_of_previous_epochs = get_number_of_trained_epochs(agent_name)
+            agent_name = os.path.join(agent_name, agent_name + "-{}".format(number_of_previous_epochs))
+
+        number_of_previous_epochs += 1
+        if type == "PPO":
+            model = PPO.load(os.path.join("agents", agent_name), env)
+    else:
+        number_of_previous_epochs = 0
+        if type == "PPO":
+            policy = config['model']['policy']
+            model = PPO(policy, env)
+    return model, number_of_previous_epochs
