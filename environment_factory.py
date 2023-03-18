be58925cc40c3d3ae95d797df766d055382a6d16
Daniel Gebhard <daniel.gebhard00@gmx.de>
2023-03-18 09:33:10 -0700

    modified observation to last 4 images

--- environment_factory.py
+++ environment_factory.py
@@ -2,6 +2,7 @@
 This file implements the get_environment function called by main.py
 """
 import flappy_bird_gym
+from gym.wrappers import FrameStack, GrayScaleObservation
 
 
 def get_environment(config):
@@ -10,4 +11,5 @@ def get_environment(config):
     :return env: a gym environment, following the specifications given in config
     """
     env = flappy_bird_gym.make("FlappyBird-rgb-v0")
+    env = FrameStack(GrayScaleObservation(env), num_stack=4)
     return env
